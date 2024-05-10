55c6cbda81dc552d95e1fc7152794861e49ec680
Abhinav hazarika <abhinavhazarika27@gmail.com>
2024-05-10 14:41:15 +0530

    xyz

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,88 +1,95 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function Calculator() {
   const [input, setInput] = useState("");
   const [result, setResult] = useState("");
 
-  const handlebutton = (value) => {
+  const handleButton = (value) => {
     setInput(input + value);
   };
-  const Calculate = () => {
-    if (input == "") {
-      setResult("Error");
-    } else
-      try {
-        setResult(eval(input));
-      } catch (error) {
-        setResult(error.message);
+
+  const calculate = () => {
+    try {
+      if (input.trim() === "") {
+        setResult("Error");
+        return;
       }
+      const calculatedResult = eval(input);
+      if (isNaN(calculatedResult)) {
+        setResult("Error");
+      } else {
+        setResult(calculatedResult);
+      }
+    } catch (error) {
+      setResult("Error");
+    }
   };
 
-  const Clear = () => {
+  const clear = () => {
     setInput("");
     setResult("");
   };
+
   return (
     <div>
       <h2>React Calculator </h2>
-      <input value={input} />
+      <input value={input} disabled />
 
       <h4> {result} </h4>
 
       <div>
-        <button className="button" onClick={() => handlebutton(7)}>
+        <button className="button" onClick={() => handleButton(7)}>
           7
         </button>
-        <button className="button" onClick={() => handlebutton(8)}>
+        <button className="button" onClick={() => handleButton(8)}>
           8
         </button>
-        <button className="button" onClick={() => handlebutton(9)}>
+        <button className="button" onClick={() => handleButton(9)}>
           9
         </button>
-        <button className="button" onClick={() => handlebutton("+")}>
+        <button className="button" onClick={() => handleButton("+")}>
           +
         </button>
         <br />
-        <button className="button" onClick={() => handlebutton(4)}>
+        <button className="button" onClick={() => handleButton(4)}>
           4
         </button>
-        <button className="button" onClick={() => handlebutton(5)}>
+        <button className="button" onClick={() => handleButton(5)}>
           5
         </button>
-        <button className="button" onClick={() => handlebutton(6)}>
+        <button className="button" onClick={() => handleButton(6)}>
           6
         </button>
-        <button className="button" onClick={() => handlebutton("-")}>
+        <button className="button" onClick={() => handleButton("-")}>
           -
         </button>
         <br />
 
-        <button className="button" onClick={() => handlebutton(1)}>
+        <button className="button" onClick={() => handleButton(1)}>
           1
         </button>
-        <button className="button" onClick={() => handlebutton(2)}>
+        <button className="button" onClick={() => handleButton(2)}>
           2
         </button>
-        <button className="button" onClick={() => handlebutton(3)}>
+        <button className="button" onClick={() => handleButton(3)}>
           3
         </button>
-        <button className="button" onClick={() => handlebutton("*")}>
+        <button className="button" onClick={() => handleButton("*")}>
           *
         </button>
         <br />
 
-        <button className="button" onClick={Clear}>
+        <button className="button" onClick={clear}>
           C
         </button>
-        <button className="button" onClick={() => handlebutton(0)}>
+        <button className="button" onClick={() => handleButton(0)}>
           0
         </button>
-        <button className="button" onClick={Calculate}>
+        <button className="button" onClick={calculate}>
           =
         </button>
-        <button className="button" onClick={() => handlebutton("/")}>
+        <button className="button" onClick={() => handleButton("/")}>
           /
         </button>
       </div>
